2910cf845000154465742d5fef0a671ec5aba493
Fanney Kristjánsdóttir <fanneykristjansdottir@Fanneys-MacBook-Air.local>
2017-02-19 19:43:22 +0000

    SQLite Db for form entries

---
+++ Borgarverk.Core/Services/FormEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MvvmCross.Plugins.Sqlite;
+using SQLite;
+
+namespace Borgarverk.Core
+{
+	public class FormEntry
+	{
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+		public string Car { get; set; }
+		public string Station { get; set; }
+		public string RoadLength { get; set; }
+		public string RoadArea { get; set; }
+		public string TarQty { get; set; }
+		public string QtyRate { get; set; }
+		
+		public override string ToString()
+		{
+			return string.Format("{0} - {1}, {2}", Id, Car, Station);
+		}
+	}
+}
